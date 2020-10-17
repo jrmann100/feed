9002f0ebc129bf16b9163562e01cdbb1f8fc65d8
jrmann100 <jordaloo@gmail.com>
2020-10-17 23:00:38 +0000

    Deploying to pages from  @ 1108f0e8dd1bda350e769e222fda19d42900b2f5 🚀

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,23 @@
 self.addEventListener('install', (event) => {
-    // Perform install steps
-});  
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                './build/bundle.js',
+                './index.html',
+                './global.css',
+                './feed.webmanifest',
+                'https://apis.google.com/js/api:client.js',
+                'https://unpkg.com/ical.js@1.4.0/build/ical.js',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
