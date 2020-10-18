c6ea2328c54ec28bf5a62daf7c45e778217c329d
jrmann100 <jordaloo@gmail.com>
2020-10-18 20:37:45 +0000

    Deploying to pages from  @ aef420ff83fb15a7a06fb86f0dbc7574bb0de5c0 🚀

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('v1').then((cache) => {
+        caches.open('dynamic').then((cache) => {
             return cache.addAll([
                 './build/bundle.js',
                 './index.html',
@@ -13,11 +13,30 @@ self.addEventListener('install', (event) => {
     );
 });
 
-self.addEventListener('fetch', function (e) {
-    console.log(e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
-    );
+/* This is the Firefox boilerplate. */
+// self.addEventListener('fetch', e => {
+//     console.log(e.request.url);
+//     e.respondWith(
+//         caches.match(e.request).then(function (response) {
+//             return response || fetch(e.request);
+//         })
+//     );
+// });
+
+/* Much better version: https://jakearchibald.com/2014/offline-cookbook/#stale-while-revalidate */
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(async function () {
+        const cache = await caches.open('dynamic');
+        const cachedResponse = await cache.match(event.request);
+        const networkResponsePromise = fetch(event.request);
+
+        event.waitUntil(async function () {
+            const networkResponse = await networkResponsePromise;
+            await cache.put(event.request, networkResponse.clone());
+        }());
+
+        // Returned the cached response if we have one, otherwise return the network response.
+        return cachedResponse || networkResponsePromise;
+    }());
 });
